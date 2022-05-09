5fbbfe2c1548eeba5c0e53885d88dc0a5fe1b172
“Jaroslav <jiha-if@dnanexus.com>
2022-05-10 01:20:42 +0200

    update deployment config

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
-if [[ $# != 1 ]]; then
-	echo "usage: $0 <image_name>"
+if [[ $# > 1 || $# < 0 ]]; then
+	echo "usage: $0 ?<image_name>"
 	exit -1
 fi
 
+if [[ $# == 1 ]]; then
+	IMAGE_NAME="$1"
 
-IMAGE_NAME="$1"
+	set -ex
 
-set -ex
+	# remove data files
+	rm -rf _trace _workflows .nextflow* db.json 
 
-# remove data files
-rm -rf _trace _workflows .nextflow* db.json 
+	# build docker image
+	docker build -t ${IMAGE_NAME} .
+	docker push ${IMAGE_NAME}
+fi
 
-# build docker image
-docker build -t ${IMAGE_NAME} .
-docker push ${IMAGE_NAME}
-
-
+minikube delete --all
 minikube start
+minikube addons enable ingress
 minikube addons enable metrics-server
 
 # deploy helm chart to kubernetes cluster 
-helm uninstall nextflow-api
+helm uninstall nextflow-api || echo 'first install'
 helm install nextflow-api ./helm
 
 kubectl create rolebinding default-edit --clusterrole=edit --serviceaccount=default:default
 kubectl create rolebinding default-view --clusterrole=view --serviceaccount=default:default
 
-minikube dashboard
 minikube service nextflow-api --url 
